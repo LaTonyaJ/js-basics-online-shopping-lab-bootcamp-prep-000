594ebc124b1d97e523f86e4e7b2925eebc49d6e0
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-08-27 21:03:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,30 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemName = item
+ var itemPrice = Math.floor(Math.random() * Math.floor(100))
+ cart.push({itemName: itemName, itemPrice: itemPrice})
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
-}
+  if(cart.length === 0){
+    return 'Your shopping cart is empty.'
+  }
+  else
+    for(let i = 0; i < cart.length; i++){
+      var items = []
+      for(var item in cart){
+        items.push(`${cart[item].itemName} at $${cart[item].itemPrice}`)}
+      }
+      if(cart.length == 1)
+       return `In your cart, you have ${items}.` 
+      if(cart.length > 1)
+       
+    
+      }
+
 
 function total() {
   // write your code here
